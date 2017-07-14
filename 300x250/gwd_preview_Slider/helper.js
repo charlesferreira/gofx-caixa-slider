92f88795bba6d69a379d762c52610cb4700f02f9
Charles Ferreira <charles.dev@live.com>
2017-07-14 15:43:42 -0300

    Remove diretórios de preview.

--- 300x250/gwd_preview_Slider/helper.js
+++
@@ -1,7 +0,0 @@
-window.helper = window.helper || {};
-
-helper.transform = function(obj, transform) {
-  obj.style.WebkitTransform = transform;
-  obj.style.msTransform = transform;
-  obj.style.transform = transform;
-}
